954246838d914f4294254457684379ab4b3709a0
Kelaiya <kelaiyarao1@gmail.com>
2018-01-28 23:44:34 -0500

    added semicolon

--- server/index.js
+++ server/index.js
@@ -14,14 +14,14 @@ app.use(morgan('dev'));
 // Sends index.html
 app.use('*', (req, res, next) => {
   res.sendFile(path.join(__dirname, '..', 'public/index.html'));
-})
+});
 
 // Error handling endware
 app.use((err, req, res, next) => {
   console.error(err)
   console.error(err.stack)
   res.status(err.status || 500).send(err.message || 'Internal server error.')
-})
+});
 
 // Start listening 
 app.listen(8080, () => console.log('Mixing it up on port 8080'))
